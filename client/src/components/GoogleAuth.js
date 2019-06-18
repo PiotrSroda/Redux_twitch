f7bb767e6883b9d5867eedd436d388195d1c0d5f
PiotrSroda <Piotr.w.sroda@gmail.com>
2019-06-18 10:29:52 +0200

    second commit

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "991242786600-arqp7abt5qdhp6g5ps4rsb5jjkt5eg9l.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
